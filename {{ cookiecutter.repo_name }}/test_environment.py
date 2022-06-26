06abf6e65c90a34b5e4e74b2a123d57496b7cb49
marcalph <marcalph@mbp.local>
2022-06-26 18:04:03 +0200

    initial commit

---
+++ {{ cookiecutter.repo_name }}/test_environment.py
@@ -0,0 +1,19 @@
+import sys
+
+REQUIRED_PYTHON = "{{ cookiecutter.python_interpreter }}"
+
+
+def main():
+    system_major = sys.version_info.major
+    required_major = 3
+
+    if system_major != required_major:
+        raise TypeError(
+            "This project requires Python {}. Found: Python {}".format(
+                required_major, sys.version))
+    else:
+        print(">>> Development environment passes all tests!")
+
+
+if __name__ == '__main__':
+    main()
